f9dec8ca03caead2fb0342fdb71680312549b217
Kat Thornton <katherine.thornton@yale.edu>
2017-09-22 12:16:29 -0400

    Update softPop.py

--- softPop.py
+++ softPop.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # 
-#   FILE: softTransform.py
-#   DATE: 9.7.17
-#   Author: Euan Cochrane
+#   FILE: softPop.py
+#   DATE: 9.22.17
+#   Author: Katherine Thornton and Kenny Seals-Nutt
 #
 
 #This is a program written in Python3 that will create new items and statements in Wikidata from CSV of software data.
